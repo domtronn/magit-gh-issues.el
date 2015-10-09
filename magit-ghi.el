4bb5781c17d6ff38c8fd0e0a49c574fb65683df3
Dom Charlesworth <dgc336@gmail.com>
2015-10-09 20:22:44 +0100

    Added the license

--- magit-ghi.el
+++ magit-ghi.el
@@ -1,3 +1,33 @@
+;;; magit-gh-issues.el --- GitHub issues extension for Magit
+
+;; Copyright (C) 2015  Dom Charlesworth
+
+;; Author: Dom Charlesworth <dgc336@gmail.com>
+;; Keywords: git tools
+;; Package-Version: 20150926.2354
+;; Version: 0.0.1
+;; URL: https://github.com/sigma/magit-gh-pulls
+;; Package-Requires: ((emacs "24") (gh "0.9.1") (magit "2.1.0") (pcache "0.2.3") (s "1.6.1"))
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+;;; Commentary:
+
+;;; Code:
+
 (require 'magit)
 (require 'gh)
 (require 'gh-issues)
