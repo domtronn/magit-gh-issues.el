9655fe3fb9bf26bff735553ad0faa376d2c90f3e
Dom Charlesworth <dgc336@gmail.com>
2015-10-10 14:50:46 +0100

    Added the ability to add labels to an issue
    It uses popup-menu with the propertized labels

--- magit-gh-issues.el
+++ magit-gh-issues.el
@@ -6,7 +6,7 @@
 ;; Keywords: git tools
 ;; Package-Version: 20150926.2354
 ;; Version: 0.0.1
-;; URL: https://github.com/sigma/magit-gh-pulls
+;; URL: https://github.com/domtronn/magit-gh-issues
 ;; Package-Requires: ((emacs "24") (gh "0.9.1") (magit "2.1.0") (pcache "0.2.3") (s "1.6.1"))
 
 ;; This file is free software; you can redistribute it and/or modify
@@ -86,20 +86,6 @@ The format should be `magit-gh-user-repo-label-name-face`"
 				 (proj (cdr repo)))
 		(oref (gh-issues-issue-list api user proj) :data)))
 
-(defun magit-gh-issues-reload ()
-  "Reload the issues by clearing the cache and fetching both
-the labels and the issues (which will set them in the cache).
-
-It the refreshes magit status to re-render the issues section."
-  (interactive)
-  (let ((repo (magit-gh-issues--guess-repo)))
-    (if (not (and repo (car repo) (cdr repo)))
-        (message "Remote repository is not configured or incorrect.")
-      (magit-gh-issues-purge-cache)
-      (magit-gh-issues-get-labels)
-      (magit-gh-issues-get-issues)
-      (magit-refresh))))
-
 (defun magit-gh-issues-purge-cache ()
   "Purge the cache of all items for a repo matching the current
 repos user and repo name."
@@ -129,12 +115,6 @@ repos user and repo name."
   "Check whether labels have been cached in API for USER & PROJ."
   (magit-gh--cached-p "labels" api user proj))
 	  
-(defun magit-gh-issues-visit-issue ()
-  "Get the URL meta data of the current issue and visit it in a browser."
-  (interactive)
-  (let ((url (magit-section-value (magit-current-section))))
-    (when (yes-or-no-p (format "Would you like to open %s in a browser?" url))
-      (browse-url url))))
 
 (defvar magit-issue-section-map
   (let ((map (make-sparse-keymap)))
@@ -156,13 +136,11 @@ repos user and repo name."
   "Create a propertized list of LABELS using faces namespaced by USER & PROJ."
   (let ((result '()))
     (dolist (label labels)
-      (message "%s" label)
       (let* ((name (cdr (assoc 'name label)))
              (s (format "%s"
                         (propertize
                          (format " %s " name)
                          'face (intern (magit-gh--build-face-name user proj name))))))
-        (message "%s" result)
         (setq result (append result (list s)))))
     result))
 
@@ -194,6 +172,43 @@ of labels specific to that GitHub project."
     (magit-gh-issues--unmarkdown-body body) 120)
    'face 'magit-dimmed))
 
+(defun magit-gh-issues-visit-issue ()
+  "Get the URL meta data of the current issue and visit it in a browser."
+  (interactive)
+  (let ((url (cdr (assoc 'url (magit-section-value (magit-current-section))))))
+    (when (yes-or-no-p (format "Would you like to open %s in a browser?" url))
+      (browse-url url))))
+
+(defun magit-gh-issues-reload ()
+  "Reload the issues by updating the cache of issues and labels.
+It refreshes magit status to re-render the issues section."
+  (interactive)
+  (let ((repo (magit-gh-issues--guess-repo)))
+    (if (not (and repo (car repo) (cdr repo)))
+        (message "Remote repository is not configured or incorrect.")
+      (magit-gh-issues-purge-cache)
+      (magit-gh-issues-get-labels)
+      (magit-gh-issues-get-issues)
+      (magit-refresh))))
+
+(defun magit-gh-issues-add-label ()
+  "Add a label from a popup menu to the current issue and refresh."
+  (interactive)
+  (when (eq 'issue (magit-section-type (magit-current-section)))
+    (let* ((repo (magit-gh-issues--guess-repo))
+           (labels (mapcar
+                    (lambda (l) (list (cons 'name (format "%s" (gh-issues--label-name l)))))
+                    (magit-gh-issues-get-labels)))
+           (prompt (magit-gh-issues--label-list labels (car repo) (cdr repo))))
+      (let* ((label (replace-regexp-in-string "^ \\(.*\\) $" "\\1" (format "%s" (popup-menu* prompt))))
+             (issue (magit-section-value (magit-current-section)))
+             (url (cdr (assoc 'url issue)))
+             (id (cdr (assoc 'id issue))))
+        (gh-issues-labels-add-to-issue
+         (magit-gh-issues--get-api) (car repo) (cdr repo)
+         id (list label))
+        (magit-gh-issues-reload)))))
+
 (defun magit-ghi-insert-ghi ()
   "Insert the actual issues sections into magit."
 	(let* ((repo (magit-gh-issues--guess-repo))
@@ -205,26 +220,25 @@ of labels specific to that GitHub project."
          (labels-cached? (magit-gh-labels-cached-p api user proj))
          (labels (when labels-cached? (magit-gh-issues-get-labels)))
          (label-string nil))
-    
-		(when (> (length labels) 0)
+		
+    (when (> (length labels) 0)
       (magit-gh-issues--make-label-faces labels user proj))
 
     (when (or (not issues-cached?) (> (length issues) 0))
 			(magit-insert-section (issues)
 				(magit-insert-heading "Issues:")
 				(dolist (issue issues)
-					(let* ((body (oref issue :body))
+					(let* ((id (oref issue :number))
+                 (body (oref issue :body))
                  (url (oref issue :html-url))
 								 (open (string= "open" (oref issue :state)))
 								 (labels (oref issue :labels))
                  (label-string (when labels
                                  (magit-gh-issues--make-label-string labels user proj))))
               (when open
-                (magit-insert-section (issue url t)
+                (magit-insert-section (issue `((url . ,url) (id . ,id)) t)
                   (magit-insert (magit-gh-issues--make-heading-string
-                                 (oref issue :number)
-                                 (oref issue :title)
-                                 label-string))
+                                 id (oref issue :title) label-string))
                   (magit-insert-heading)
                   (when body
                     (magit-insert-section (body)
