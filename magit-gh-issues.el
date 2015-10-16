33f49a5557d35d85f99ff7b960b9e8613ac8ee8c
Dom Charlesworth <dgc336@gmail.com>
2015-10-16 10:50:45 +0100

    Wipe out the contents of the comment buffer #28
    This is again quite hacky, but it should prevent the comment being created

--- magit-gh-issues.el
+++ magit-gh-issues.el
@@ -285,6 +285,8 @@ It refreshes magit status to re-render the issues section."
 (define-derived-mode ghi-comment-mode markdown-mode "Git Comment"
   "Major mode for editing GHI Comment files."
   :group 'magit
+  (add-hook 'with-editor-pre-cancel-hook
+            '(lambda () (with-current-buffer (buffer-name) (goto-char (point-min)) (flush-lines ""))) nil t)
   (with-editor-mode 1))
 
 (defun magit-gh-issues--reload-when-in-magit ()
